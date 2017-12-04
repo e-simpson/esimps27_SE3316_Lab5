3579ce0bec1d7a9cea9f14bf20ab2a16701a18ba
Evan Simpson <esimps27@uwo.ca>
2017-12-04 23:41:25 +0000

    removed unused code

--- server/server.js
+++ server/server.js
@@ -394,44 +394,6 @@ router.route('/dmca')
 
 
 
-
-
-
-
-
-// router.post('/login', function(req, res, next) {
-//   Passport.authenticate('local', function(err, user, info) {
-//     if (err) {
-//       return next(err);
-//     }
-//     if (!user) {
-//       return res.status(401).json({
-//         err: info
-//       });
-//     }
-//     req.logIn(user, function(err) {
-//       if (err) {
-//         return res.status(500).json({
-//           err: 'Could not log in user'
-//         });
-//       }
-//       res.status(200).json({
-//         status: 'Login successful!'
-//       });
-//     });
-//   })(req, res, next);
-// });
-
-// router.get('/logout', function(req, res) {
-//   req.logout();
-//   res.status(200).json({
-//     status: 'Bye!'
-//   });
-// });
-//=====================================
-
-
-
 router.use(function(req, res, next) {
     console.log('Something is happening');
     next();
